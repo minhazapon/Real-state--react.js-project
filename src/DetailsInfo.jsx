57249cef1511cfc5ead68ff0604036a9bba96e0f
minhazapon <minhajulabedinapon@gmail.com>
2024-04-10 01:14:50 -0700

    details file

---
+++ src/DetailsInfo.jsx
@@ -0,0 +1,61 @@
+
+
+const DetailsInfo = ({details}) => {
+
+   const {Image, estate_title, name, description, price, 
+      
+    Status, Area, location, facilities
+
+         } = details
+
+
+    return (
+        <div>
+            <div>
+
+
+
+            <div className=" bg-base-200 border-[1px] border-blue-600 rounded-2xl mt-5 ml-5 mr-5  ">
+              <div className="hero-content flex-col lg:flex-row">
+                <img  src={Image} className=  "  h-[300px] w-[300px]  max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                  <h1 className="text-5xl font-bold ">{estate_title}</h1>
+
+                  <p className="py-6  text-xl"> {description} </p>
+                  <p  className=" text-xl font-bold"  >Name: {name}  </p>
+              
+                  <p  className=" text-xl font-bold"  >Name: {name}  </p>
+                  <p  className=" text-xl font-bold"  >Area: {Area}  </p>
+                  <p  className=" text-xl font-bold"  >location: {location}  </p>
+                  <p  className=" text-xl font-bold"  >facilities: {facilities}  </p>
+
+                 
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default DetailsInfo;
--- src/EstateDetails.jsx
+++ src/EstateDetails.jsx
@@ -1,11 +1,42 @@
 
+import { useEffect } from "react";
+import { useState } from "react";
+import DetailsInfo from "./DetailsInfo";
+
 
 const EstateDetails = () => {
+
+   const [details, setDetails] = useState([])
+
+   useEffect( () => {
+      fetch('estate.json')
+      .then(res => res.json())
+      .then(data => setDetails(data))
+   }  , [])
+
+
+ 
+
+
     return (
         <div>
 
-            <h1>Show details</h1>
+              <div className=" text-center  mb-10 mt-5">
 
+                  <h1 className=" text-6xl font-bold">Our Estate Full Information</h1>
+
+              </div>
+            
+            <div className=" mb-10 mt-10">
+
+                 {
+                    details.map( details => <DetailsInfo key={details.id} details={details}></DetailsInfo> )
+
+                 }
+
+
+
+            </div>
 
 
             
